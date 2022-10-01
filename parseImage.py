e09b2129426cfd4bbfa1ef5e53b399f4c5322bc3
CharFox1 <charfox1@umbc.edu>
2022-10-01 12:40:22 -0400

    color image and functions

--- parseImage.py
+++ parseImage.py
@@ -3,10 +3,18 @@
 import numpy as np
 import cv2
 
-# Load an color image in grayscale
-img = cv2.imread("images/puppy.jpg",0)
- 
-# show image
-cv2.imshow('image',img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def readImage():
+    # Load an color image in color
+    img = cv2.imread("images/puppy.jpg",1)
+    
+    # show image
+    cv2.imshow('image',img)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    return img
+
+def main():
+    img = readImage()
+
+if __name__ == "__main__":
+    main()
