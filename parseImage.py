8c27c9080c9d831e4aa37881790280117320a328
CharFox1 <charfox1@umbc.edu>
2022-10-01 12:35:36 -0400

    open test image

--- parseImage.py
+++ parseImage.py
@@ -1,1 +1,12 @@
 # do ocr stuff here to find all chars in image
+
+import numpy as np
+import cv2
+
+# Load an color image in grayscale
+img = cv2.imread("images/puppy.jpg",0)
+ 
+# show image
+cv2.imshow('image',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
